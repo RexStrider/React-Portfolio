e1a0c65f9fce683b5b1c2a7169880baccaa0be34
Odysseus <arwaterman@gmail.com>
2019-06-23 14:03:28 -0700

    adjusting styling

---
+++ client/src/Utils/Projects.js
@@ -0,0 +1,39 @@
+import logo_study_live from '../Assets/Images/study_live.png';
+import logo_flip_search from '../Assets/Images/flip_search.png';
+import logo_parent_maps from '../Assets/Images/parent_maps.png';
+
+class Project {
+    constructor(title, description, logo, github_url, demo_url) {
+        this.title = title;
+        this.description = description;
+        this.logo = logo;
+        this.github_url = github_url;
+        this.demo_url = demo_url;
+    }
+}
+
+const projects = [
+    new Project(
+        "Parent Maps",
+        "A community based database where parents can post reviews on various places.",
+        logo_parent_maps,
+        "https://github.com/RexStrider/berkeleyfamilyfriendly",
+        "https://gtresquire.github.io/berkeleyfamilyfriendly/"
+    ),
+    new Project(
+        "Study Live",
+        "A study room where students can gather, chat, and listen to music.",
+        logo_study_live,
+        "https://github.com/RexStrider/study-live",
+        "https://study-live.herokuapp.com/"
+    ),
+    new Project(
+        "Flip Search",
+        "A click to search product demo for re-selling houses in the Bay Area.",
+        logo_flip_search,
+        "https://github.com/RexStrider/flipsearch",
+        "https://flipsearch.herokuapp.com/main"
+    )
+]
+
+export default projects;
