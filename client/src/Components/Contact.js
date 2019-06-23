8842bfef1fc31ebedf1f17b0a4b4807d69d3aecd
Odysseus <arwaterman@gmail.com>
2019-06-23 12:47:06 -0700

    moved post Data function to api utilities directory

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,4 +1,5 @@
 import './Contact.css';
+import API from '../Utils/API';
 
 import React, { Component } from 'react';
 
@@ -9,12 +10,11 @@ class Contact extends Component {
         email: '',
         message: '',
         formMessage: 'Click submit to send a message.'
-        // formMessage: ''
     };
     
     clickHandler = () => {
         const url = '/api/mail';
-        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
         const data = {
             name: this.state.name,
@@ -22,9 +22,8 @@ class Contact extends Component {
             message: this.state.message
         };
 
-        // error checking on data
-        if (regex.test(data.email)) {
-            this.postData(url, data).then(response => {
+        if (regexEmail.test(data.email)) {
+            API.postData(url, data).then(response => {
                 const formMessage = response.message;
 
                 this.setState({ name: '', email: '', message: '', formMessage });
@@ -34,24 +33,6 @@ class Contact extends Component {
             this.setState({ formMessage: 'Please check your email address.' });
         }
     }
-
-    postData = (url = '', data = {}) => {
-        // Default options are marked with *
-          return fetch(url, {
-              method: 'POST', // *GET, POST, PUT, DELETE, etc.
-              mode: 'cors', // no-cors, cors, *same-origin
-              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-              credentials: 'same-origin', // include, *same-origin, omit
-              headers: {
-                  'Content-Type': 'application/json',
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-              redirect: 'follow', // manual, *follow, error
-              referrer: 'no-referrer', // no-referrer, *client
-              body: JSON.stringify(data), // body data type must match "Content-Type" header
-          })
-          .then(response => response.json()); // parses JSON response into native Javascript objects 
-      }
 
     handleInputChange = event => {
         // Getting the value and name of the input which triggered the change
