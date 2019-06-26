2ecebf5237d8b8659b34df59a2a8e75810c9f81b
Odysseus <arwaterman@gmail.com>
2019-06-26 16:57:32 -0700

    added in portfolioAddress into production keys

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     googleClientSecret: process.env.googleClientSecret,
     googleAccessToken: process.env.googleAccessToken,
     googleRefreshToken: process.env.googleRefreshToken,
+    portfolioAddress: process.env.portfolioAddress,
     portfolioPassword: process.env.portfolioPassword
 }
