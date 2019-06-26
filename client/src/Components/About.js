93720d103b45f126c9e70cd0baf16ba08ed5342c
Odysseus <arwaterman@gmail.com>
2019-06-25 17:22:31 -0700

    adjusted about bio

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,6 +1,7 @@
 import './About.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import portfolio_pic from '../Assets/Images/portfolio_pic.jpg';
 
 function About() {
@@ -9,11 +10,10 @@ function About() {
             <img src={portfolio_pic} alt="porfolio_pic" />
 
             <section>
-                <h2>Background</h2>
+                <h2>About Myself</h2>
                 <p>I am a motivated web-developer with well rounded experience in the MERN stack.</p>
                 <p>I've developed various web applications from the ground up, and worked with a team to efficiently build on a variety of projects.</p>
-                <p>If you'd like to check out some of my projects, please click on the projects link in the header.</p>
-                <p>And if you'd like to contact me, you can use the contact form I built or click the link in the footer.</p>
+                
             </section>
         </div>
     );
