fab9aaf3ec118d346bf5d041c1b5f2aac309cab0
Odysseus <arwaterman@gmail.com>
2019-06-26 16:01:10 -0700

    added route to react front end

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const router = express.Router();
 const apiRoutes = require("./api");
+const path = require('path');
+
+const router = express.Router();
+
 
 router.use("/api", apiRoutes);
 
-router.get('/', (req, res) => {
-    res.send({ message: 'Congratulations, you found the test route!'});
-});
+// Add route to react application
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    });
+  } else {
+    app.use(express.static(path.join(__dirname, '/client/public')));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "/client/public/index.html"));
+    });
+  }
 
 module.exports = router;
