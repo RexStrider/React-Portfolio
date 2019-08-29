75c9ce630faa0c7dc44cb200bcc4ddf188a50e73
Odysseus <arwaterman@gmail.com>
2019-08-29 16:10:10 -0700

    added in forest initiative project

--- client/src/Utils/Projects.js
+++ client/src/Utils/Projects.js
@@ -1,7 +1,8 @@
 import logo_study_live from '../Assets/Images/study_live.png';
 import logo_flip_search from '../Assets/Images/flip_search.png';
 import logo_parent_maps from '../Assets/Images/parent_maps.png';
-import logo_crystals_collector from '../Assets/Images/crystals_collector.png'
+import logo_crystals_collector from '../Assets/Images/crystals_collector.png';
+import logo_forest_initiative from '../Assets/Images/forest-initiative.png';
 
 class Project {
     constructor(title, description, logo, github_url, demo_url) {
@@ -14,12 +15,20 @@ class Project {
 }
 
 const projects = [
+    new Project (
+        "Forest Initiative",
+        "A one stop website for learning about the state of our wilderness and getting in contact with our representatives.",
+        logo_forest_initiative,
+        "https://github.com/RexStrider/ForestInitiative",
+        "https://forest-initiative.herokuapp.com/"
+
+    ),
     new Project(
         "Parent Maps",
         "A community based database where parents can post reviews on various places.",
         logo_parent_maps,
         "https://github.com/RexStrider/berkeleyfamilyfriendly",
-        "https://gtresquire.github.io/berkeleyfamilyfriendly/"
+        "https://rexstrider.github.io/Parent-Maps/"
     ),
     new Project(
         "Study Live",
