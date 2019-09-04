e63096f1cef6ec6a9ce9e813850d1cb5b6555e67
Odysseus <arwaterman@gmail.com>
2019-09-04 15:32:09 -0700

    added more information about technical skills

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -11,6 +11,8 @@ function About() {
             <section>
                 <h2>About Myself</h2>
                 <p>I am a motivated web-developer with well rounded experience in the MERN stack.</p>
+                <p>I have been developing with JavaScript, working with Node and Express to develop the back-end, and React to develop the front-end.</p>
+                <p>I've used both SQL and MongoDB to set a database on the back-end.</p>
                 <p>I've developed various web applications from the ground up, and worked with a team to efficiently build on a variety of projects.</p>
                 <p>If you'd like to collaborate on a project, then send me a message and I'll get back to you as soon as possible.</p>
             </section>
